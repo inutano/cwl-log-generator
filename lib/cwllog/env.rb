0d7ee8d0ef41ebcd0335596bfb29c7cd2725bc66
tazro inutano ohta <inutano@gmail.com>
2018-05-21 13:33:32 +0900

    get environment info

--- lib/cwllog/env.rb
+++ lib/cwllog/env.rb
@@ -1,4 +1,35 @@
 module CWLlog
   class Env
+    def generate
+      env_types = [
+        env,
+        CWLlog::Env::AWS.generate,
+      ]
+      env_types.inject(&:merge)
+    end
+
+    def env
+      {
+        hostname: get_hostname,
+        total_memory: get_total_memory,
+        disk_size: get_disk_size,
+      }
+    end
+
+    def get_system_info(cmd)
+      `#{cmd}`.chomp
+    end
+
+    def get_hostname
+      get_system_info("hostname")
+    end
+
+    def get_total_memory
+      get_system_info("grep ^MemTotal /proc/meminfo | awk '{ print $2 }'")
+    end
+
+    def get_disk_size
+      get_system_info("df -k --output=size / | awk 'NR==2 { print $1 }'")
+    end
   end
 end
