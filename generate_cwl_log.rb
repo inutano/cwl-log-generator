54cb40e63acc064fda6166b80db09e665bcf81b9
tazro inutano ohta <inutano@gmail.com>
2018-05-22 12:37:25 +0900

    getoptlong

--- generate_cwl_log.rb
+++ generate_cwl_log.rb
@@ -5,14 +5,32 @@
 $LOAD_PATH << __dir__
 $LOAD_PATH << File.join(__dir__, "lib")
 
+require 'getoptlong'
 require 'lib/cwllog'
 
 if __FILE__ == $0
-  CWLlog::CWL::DebugInfo.load(ARGV[0]) # --debug output with timestamps
-  CWLlog::CWL::JobConf.load(ARGV[1]) # job conf yaml or json
-  CWLlog::CWL::DebugInfo.cidfile_dir(ARGV[2]) # --debug output with timestamps
-  CWLlog::Docker.load_docker_ps(ARGV[3])
-  CWLlog::Docker.load_docker_info(ARGV[4])
+  opts = GetoptLong.new(
+    ['--debug-output', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--job-conf', GetoptLong::REQUIRED_ARGUMENT],
+    ['--cidfile-dir', GetoptLong::REQUIRED_ARGUMENT],
+    ['--docker-ps', GetoptLong::OPTIONAL_ARGUMENT],
+    ['--docker-info', GetoptLong::OPTIONAL_ARGUMENT],
+  )
+
+  opts.each do |opt, arg|
+    case opt
+    when '--debug-output'
+      CWLlog::CWL::DebugInfo.load(arg) # --debug output with timestamps
+    when '--job-conf'
+      CWLlog::CWL::JobConf.load(arg) # job conf yaml or json
+    when '--cidfile-dir'
+      CWLlog::CWL::DebugInfo.cidfile_dir(arg) # --debug output with timestamps
+    when '--docker-ps'
+      CWLlog::Docker.load_docker_ps(arg)
+    when '--docker-info'
+      CWLlog::Docker.load_docker_info(arg)
+    end
+  end
 
   puts CWLlog.generate
 end
