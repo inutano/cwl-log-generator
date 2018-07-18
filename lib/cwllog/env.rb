4128f29a8b1452be5b253ef3760338c9c8556208
Tomoya Tanjo <ttanjo@gmail.com>
2018-07-18 17:44:52 +0900

    Change the type of memory size and disk size to integers

--- lib/cwllog/env.rb
+++ lib/cwllog/env.rb
@@ -36,16 +36,16 @@ module CWLlog
       def get_total_memory
         case RUBY_PLATFORM
         when /linux/
-          get_system_info("grep ^MemTotal /proc/meminfo | awk '{ print $2 }'")
+          get_system_info("grep ^MemTotal /proc/meminfo | awk '{ print $2 }'").to_i
         when /darwin/
-          get_system_info("sysctl hw.memsize | awk '{ print $2 }'")
+          get_system_info("sysctl hw.memsize | awk '{ print $2 }'").to_i
         else
           # unsupported
         end
       end
 
       def get_disk_size
-        get_system_info("df -k / | awk 'NR==2 { print $2 }'")
+        get_system_info("df -k / | awk 'NR==2 { print $2 }'").to_i
       end
     end
   end
